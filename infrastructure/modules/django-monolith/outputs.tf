8694a77ef80b557648f3e4aa09fc71fe81c725ab
kwatras <58006971+ChrisW-priv@users.noreply.github.com>
2026-04-14 00:22:39 +0200

    fix: cloud function deployment (#84)
    bulk commit of the fixes to the infra that support publishing of the
    cloud functions.

--- infrastructure/modules/django-monolith/outputs.tf
+++ infrastructure/modules/django-monolith/outputs.tf
@@ -37,3 +37,23 @@ output "mcp_image_url" {
   description = "The MCP Docker image URL"
   value       = local.mcp_image_url
 }
+
+output "db_private_ip" {
+  description = "The private IP address of the Cloud SQL instance"
+  value       = module.postgres_db.private_ip_address
+}
+
+output "db_password_secret_id" {
+  description = "Secret Manager secret ID for the DB password"
+  value       = module.postgres_db.db_password_secret_id
+}
+
+output "db_user" {
+  description = "The Cloud SQL admin username"
+  value       = module.postgres_db.db_admin_user
+}
+
+output "db_name" {
+  description = "The Cloud SQL database name"
+  value       = module.postgres_db.db_name
+}
